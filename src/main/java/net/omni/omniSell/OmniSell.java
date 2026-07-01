4fe89eb41d046de044ee6442da90d9a82223db18
Aldrin Victoria <victoriaaldrin812@gmail.com>
2026-07-01 20:39:17 +0800

    Initial Commit

---
+++ src/main/java/net/omni/omniSell/OmniSell.java
@@ -0,0 +1,16 @@
+package net.omni.omniSell;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OmniSell extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
