4fc35d708505b531620e2021553a5e45615f3a99
domninos <domninosdev@gmail.com>
2026-07-03 16:05:04 +0800

    feat: messages

---
+++ src/main/java/net/omni/sell/managers/MessagesManager.java
@@ -0,0 +1,51 @@
+package net.omni.sell.managers;
+import net.omni.sell.OmniSell;
+import net.omni.sell.util.Messages;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.List;
+
+public class MessagesManager {
+    private final OmniSell plugin;
+
+    public MessagesManager(OmniSell plugin) {
+        this.plugin = plugin;
+    }
+
+    public void loadMessages() {
+        plugin.getMessagesConfig().reload();
+
+        FileConfiguration config = plugin.getMessagesConfig().getConfig();
+
+        int savedDefaults = 0;
+
+        for (Messages message : Messages.values()) {
+            if (message.getDefaultVal() instanceof List<?>) {
+                if (!config.contains(message.getPath())) {
+                    config.set(message.getPath(), message.getDefaultVal());
+                    savedDefaults++;
+                }
+
+                message.setCachedVal(plugin.getConfig().getStringList(message.getPath()));
+            } else {
+                if (!config.contains(message.getPath())) {
+                    config.set(message.getPath(), message.getDefaultVal());
+                    savedDefaults++;
+                }
+
+                message.setCachedVal(config.getString(message.getPath()));
+            }
+        }
+
+        if (savedDefaults > 0) {
+            plugin.getMessagesConfig().save();
+
+            plugin.sendConsole("<green>Successfully loaded " + savedDefaults + " default message(s).</green>");
+        }
+    }
+
+    public void flush() {
+        for (Messages message : Messages.values())
+            message.flush();
+    }
+}
---
+++ src/main/java/net/omni/sell/util/Messages.java
@@ -0,0 +1,100 @@
+package net.omni.sell.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum Messages {
+    NO_PERMS("no_perms", "<red>You do not have permission to use this command.</red>"),
+    ONLY_PLAYERS("only_players", "<red>Only players can use this command.</red>"),
+
+    PLAYER_NOT_FOUND("player_not_found", "<red>Player %player% not found.</red>"),
+    USAGE("usage", "<red>Invalid arguments. Usage: %usage%</red>"),
+    UNKNOWN_COMMAND("unknown_cmd", "<red>Unknown command.</red>"),
+
+    GIVE_SUCCESS("give_success", "<green>Successfully gave %player% a chunk hopper."),
+    GIVE_ERROR("give.error", "<red>Could not give %player% a chunk hopper. Please check logs.</red>"),
+
+    RELOADED("reloaded", "<green>config.yml and messages.yml have been reloaded.</green>");
+
+    private final String path;
+    private final Object defaultVal;
+    private Object cachedVal;
+
+    Messages(String path, Object defaultVal) {
+        this.path = path;
+        this.defaultVal = defaultVal;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public Object getDefaultVal() {
+        return defaultVal;
+    }
+
+    public void setCachedVal(Object val) {
+        this.cachedVal = val;
+    }
+
+    public String replace(String... pairs) {
+        String result = this.toString();
+
+        return replace(result, pairs);
+    }
+
+    @Override
+    public String toString() {
+        if (cachedVal instanceof List<?>)
+            return "";
+
+        return cachedVal instanceof String ? (String) cachedVal : (String) defaultVal;
+    }
+
+    private String replace(String result, String... pairs) {
+        if (result.isEmpty())
+            return "";
+
+        for (int i = 0; i < pairs.length - 1; i += 2) {
+            String key = pairs[i];
+            String val = pairs[i + 1];
+
+            if (key != null && val != null) {
+                result = result.replace("%" + key + "%", val);
+            }
+        }
+
+        return result;
+    }
+
+    public String replaceList(String... pairs) {
+        List<String> originalList = this.asList();
+
+        if (originalList.isEmpty())
+            return "";
+
+        List<String> modifiedList = new ArrayList<>();
+
+        for (String line : originalList) {
+            if (line != null)
+                modifiedList.add(replace(line, pairs));
+        }
+
+        return String.join("\n", modifiedList);
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<String> asList() {
+        return cachedVal instanceof List<?> ? (List<String>) cachedVal : (List<String>) defaultVal;
+    }
+
+    public void flush() {
+        if (cachedVal instanceof List<?> cachedList)
+            cachedList.clear();
+
+        if (defaultVal instanceof List<?> defaultList)
+            defaultList.clear();
+
+        this.cachedVal = null;
+    }
+}
